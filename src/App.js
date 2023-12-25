755b22b0263ce4e7825dbd9bdd833f31831066bc
Nnachi-clinton <clinton.amadi@decagon.dev>
2023-12-25 12:26:52 +0100

    project setup completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ErrorBoundary from "../src/components/common/ErrorBoundary.js";
+import AuthState from "../src/context/auth/AuthState.js";
+import { GlobalProvider } from "../src/context/GlobalState.js";
+import HomePage from "../src/pages/Home.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ErrorBoundary>
+      <Suspense fallback="loading">
+        <Router>
+          <GlobalProvider>
+            <AuthState>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+              </Routes>
+            </AuthState>
+          </GlobalProvider>
+        </Router>
+      </Suspense>
+    </ErrorBoundary>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+// Home.js
+import React from "react";
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to My SmartQuiz React App</h1>
+
+      {/* Add your content for the home page */}
+    </div>
+  );
+};
+
+export default HomePage;
